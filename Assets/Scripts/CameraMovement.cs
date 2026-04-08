2797191bec250ced4b758a2b6b852712a98dbce4
Janik <xy2045@icloud.com>
2026-04-08 13:43:18 +0200

    Adds camera to follow player

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -2,20 +2,21 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    private Vector3 pos = new Vector3(0, 10, 0);
+    private Vector3 offset = new Vector3(0, 10, 0);
     // Rotate for Top-Down-View
     private Quaternion rotation = Quaternion.Euler(90, 0, 0);
+    public GameObject player;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.position = pos;
+        transform.position = player.transform.position + offset;
         transform.rotation = rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = player.transform.position + offset;
     }
 }
