6a169b25599d48c7931fdaba92844d7bdbb7108a
Kaisabu <qet71seq@hhu.de>
2026-04-08 16:02:37 +0200

    Make coins collectable

---
+++ Assets/Scripts/ControlCoin.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class ControlCoin : MonoBehaviour
+{
+    private float rotationSpeed = 75;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
