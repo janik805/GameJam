464d316f1c31eaab527a063dc5d12c38ed3192b7
pro470 <reynoldsadjei@gmx.de>
2026-04-08 18:35:33 +0200

    add: coin are now randomly spawn on the plane

---
+++ Assets/Scripts/CoinSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class CoinSpawner : MonoBehaviour
+{
+    public GameObject Coin;
+    public GameObject plane;
+    private float xGrenze;
+    private float zGrenze;
+    private Renderer renderer;
+
+    void Start()
+    {
+        renderer = plane.GetComponent<Renderer>();
+        xGrenze = renderer.bounds.size.x / 2;
+        zGrenze = renderer.bounds.size.z / 2;
+        InvokeRepeating(nameof(SpawnCoin), 1, 1);
+    }
+
+    public void SpawnCoin()
+    {
+        float spawnpointX = Random.Range(-xGrenze, xGrenze);
+        float spawnpointZ = Random.Range(-zGrenze, zGrenze);
+
+        Vector3 spawnPoint = new Vector3(spawnpointX, 1, spawnpointZ);
+        Instantiate(Coin, spawnPoint, Quaternion.identity);
+    }
+}
