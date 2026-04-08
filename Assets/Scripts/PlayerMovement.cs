df5f50451b1ea0366574839fdb679e7d53662fdb
Janik <xy2045@icloud.com>
2026-04-08 16:33:44 +0200

    Adds out of bounds handling to player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,17 @@
+using System;
 using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public GameObject plane;
     new Rigidbody rigidbody;
     private float horizontalInput;
     private float forwardInput;
     private int turnSpeed = 70;
     private int speed = 10;
+    private float xGrenze;
+    private float zGrenze;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -27,6 +31,35 @@ public class PlayerMovement : MonoBehaviour
             transform.Rotate(Vector3.down, Time.deltaTime * turnSpeed * horizontalInput);
         } else {
             transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        }
+        CheckLimit();
+    }
+
+    // Checks that player can't go out of Bounds
+    private void CheckLimit()
+    {
+        if (plane!=null)
+        {
+            // Get edge of ground plane
+            Renderer renderer = plane.GetComponent<Renderer>();
+            xGrenze = renderer.bounds.size.x / 2;
+            zGrenze = renderer.bounds.size.z / 2;
+
+            if  (transform.position.x >= xGrenze)
+            {
+                transform.position = transform.position - new Vector3(1, 0 , 0);
+            } else if  (Math.Abs(transform.position.x) >= xGrenze)
+            {
+                transform.position = transform.position + new Vector3(1, 0 , 0);
+            }
+
+            if  (transform.position.z >= zGrenze)
+            {
+                transform.position = transform.position - new Vector3(0, 0 , 1);
+            } else if  (Math.Abs(transform.position.z) >= zGrenze)
+            {
+                transform.position = transform.position + new Vector3(0, 0 , 1);
+            }
         }
     }
 }
