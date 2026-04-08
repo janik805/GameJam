6cbd1e24c38624b419c41ae008abb9685be5e3dd
Janik <xy2045@icloud.com>
2026-04-08 14:45:58 +0200

    Adds basic forward movement to player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,21 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    new Rigidbody rigidbody;
+    private float horizontalInput;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Vertical");
+        rigidbody.AddForce(Vector3.forward * horizontalInput, ForceMode.Impulse);
     }
 }
