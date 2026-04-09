87ed297b31f72236b96fd89ed1926c2e2a80b785
Janik <xy2045@icloud.com>
2026-04-09 12:38:55 +0200

    Adds Coin Count

--- Assets/Scripts/TextManager.cs
+++ Assets/Scripts/TextManager.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 public class TextManager : MonoBehaviour
 {   
     public TextMeshProUGUI scoreText;
-    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        scoreText.text = "Score:" + 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        scoreText.text = "Score:" + PlayerStatsManager.Instance.GetCoins();;
     }
 }
