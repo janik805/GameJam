cd76afd090f7a5b4ea695f91135330aec6770580
Janik <xy2045@icloud.com>
2026-04-09 15:43:25 +0200

    Adds Health Stat to Camera View

--- Assets/Scripts/TextManager.cs
+++ Assets/Scripts/TextManager.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 public class TextManager : MonoBehaviour
 {   
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI healthText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        scoreText.text = "Score:" + 0;
+        scoreText.text = "Score:" + PlayerStatsManager.Instance.GetCoins();
+        healthText.text = "Health:"+PlayerStatsManager.Instance.GetHealth();
     }
 
     // Update is called once per frame
     void Update()
     {
-        scoreText.text = "Score:" + PlayerStatsManager.Instance.GetCoins();;
+        scoreText.text = "Score:" + PlayerStatsManager.Instance.GetCoins();
+        healthText.text = "Health:" + PlayerStatsManager.Instance.GetHealth();
     }
 }
