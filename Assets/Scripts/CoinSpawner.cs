81d04dcf2ba32081db63839a5fe194b59214dbb4
Reynolds adjei <63075278+pro470@users.noreply.github.com>
2026-04-09 13:27:21 +0200

    add: coins now spawn on a random time and also a max value

--- Assets/Scripts/CoinSpawner.cs
+++ Assets/Scripts/CoinSpawner.cs
@@ -9,21 +9,35 @@ public class CoinSpawner : MonoBehaviour
     private float xGrenze;
     private float zGrenze;
     private new Renderer renderer;
+    private float timer;
+    private float waitTime;
 
     void Start()
     {
         renderer = plane.GetComponent<Renderer>();
         xGrenze = renderer.bounds.size.x / 2;
         zGrenze = renderer.bounds.size.z / 2;
-        InvokeRepeating(nameof(SpawnCoin), 1, 1);
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= waitTime)
+        {
+            SpawnCoin();
+            timer = 0;
+            waitTime = Random.Range(1, 10);
+        }
     }
 
     public void SpawnCoin()
     {
+        if (WorldStatsManager.Instance.getCoinsSpawned() >= WorldStatsManager.Instance.getMaxCoinsSpawned()) return;
         float spawnpointX = Random.Range(-xGrenze, xGrenze);
         float spawnpointZ = Random.Range(-zGrenze, zGrenze);
 
         Vector3 spawnPoint = new Vector3(spawnpointX, 1, spawnpointZ);
         Instantiate(Coin, spawnPoint, Quaternion.identity);
+        WorldStatsManager.Instance.increaseCoinsSpawned();
     }
 }
