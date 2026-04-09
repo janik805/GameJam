7c3bf1b7c413fa055e8f8d63525d1fa63fbfef21
Kaisabu <qet71seq@hhu.de>
2026-04-09 15:20:22 +0200

    Implement an Interface for the enemies

--- Assets/Scripts/SkeletonController.cs
+++ Assets/Scripts/SkeletonController.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 
-public class SkeletonController : MonoBehaviour
+public class SkeletonController : MonoBehaviour, EnemyInterface
 {
     private GameObject player;
     private Animator animation;
-    private Rigidbody rigidbody;
+    private new Rigidbody rigidbody;
 
     private float speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,7 +15,6 @@ public class SkeletonController : MonoBehaviour
         animation = GetComponent<Animator>();
         rigidbody = GetComponent<Rigidbody>();
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -23,11 +22,15 @@ public class SkeletonController : MonoBehaviour
 
     void FixedUpdate()
     {
+        Walk();
+    }
+
+    public void Walk() 
+    {
         Vector3 playerPosition = player.transform.position;
         Vector3 goToPlayer = (playerPosition - transform.position).normalized;
         rigidbody.AddForce(goToPlayer * speed);
         transform.rotation = Quaternion.LookRotation(goToPlayer);
-
         animation.SetFloat("speed", 1);
     }
 }
