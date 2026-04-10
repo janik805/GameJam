9d2d60859ea7083f0f581ed4b6fe5d94fb45af06
Janik <xy2045@icloud.com>
2026-04-10 11:13:18 +0200

    Stops game at start and after game over

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,15 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI gameOverText;
     public static GameManager Instance;
-    private Boolean gameOver = false;
+    private Boolean gameRunning = false;
+    private Boolean gameStarted = false;
 
     private void Awake()
     {
@@ -18,28 +20,40 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Time.timeScale = 0f; // Zeit wird beim Start gestoppt
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gameOver)
+        // Startet Spiel mit der Leertaste
+        if (!gameRunning && Input.GetKeyDown(KeyCode.Space))
         {
-            if (Input.anyKeyDown)
-            {
-                gameOver = true;
-                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-            }
+            StartGame();
+        }
+    }
+
+    private void StartGame()
+    {
+        gameRunning = true;
+        Time.timeScale = 1f;
+        if (gameStarted) 
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        } else
+        {
+            gameStarted = true;
         }
     }
 
     public void GameOver()
     {
         gameOverText.gameObject.SetActive(true);
-        gameOver = true;
+        Time.timeScale = 0f; // Stops game
+        gameRunning = false;
     }
 }
